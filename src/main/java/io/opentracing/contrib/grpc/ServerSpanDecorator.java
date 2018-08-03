e6134177a7f792f7f657ff746d687e3ce4ab8b62
Cristian Bodnar <crisbodnar96@gmail.com>
2018-08-03 12:00:01 +0100

    Add the ServerSpanDecorator interface

---
+++ src/main/java/io/opentracing/contrib/grpc/ServerSpanDecorator.java
@@ -0,0 +1,24 @@
+package io.opentracing.contrib.grpc;
+
+import io.grpc.Metadata;
+import io.grpc.ServerCall;
+import io.grpc.ServerCallHandler;
+import io.opentracing.Span;
+
+/**
+ * An interface for adding custom span tags and logs to the spans created by
+ * {@link ServerTracingInterceptor}
+ */
+public interface ServerSpanDecorator {
+
+  /**
+   * The method of the implementation is executed inside
+   * {@link ServerTracingInterceptor#interceptCall(ServerCall, Metadata, ServerCallHandler)}
+   * @param span The span created by {@link ServerTracingInterceptor}
+   * @param call The {@link ServerCall} parameter of
+   * {@link ServerTracingInterceptor#interceptCall(ServerCall, Metadata, ServerCallHandler)}
+   * @param headers The {@link Metadata} parameter of
+   * {@link ServerTracingInterceptor#interceptCall(ServerCall, Metadata, ServerCallHandler)}
+   */
+  void interceptCall(Span span, ServerCall call, Metadata headers);
+}
